2e00a76b21c4d912ec2b2e7ecd98af8cd30bb933
DavidB-29 <daviidbernal29@gmail.com>
2025-05-27 01:09:45 -0600

    agregamos funcionalidad a los 4 botones para filtrar la tabla

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const searcher = document.getElementById("buscarverbo");
 const tablee = document.getElementById("table");
 const bodytable = document.getElementsByTagName("tbody")[0];
+const filas = tablee.getElementsByTagName("tr");
+
+
 
     /*ARRAYS GLOBALES*/
 
@@ -33,8 +36,9 @@ const arrayVerbosIrregular = [
     ["Leave", "Left", "Left"],
     ["Eat", "Ate", "Eaten"]
     ]
-
 const todosLosVerbos = arrayVerbosRegular.concat(arrayVerbosIrregular);
+
+    /*ORDENA LOS VERBOS EN LA TABLA DE FORMA AUTOMATICA DE LA A-Z*/
 
 todosLosVerbos.sort(function(a, b){
     return a[0].localeCompare(b[0])
@@ -48,8 +52,9 @@ for(let filaValue of todosLosVerbos){
     };
    bodytable.appendChild(newfila);
 }
+    /*LIMPIA Y PINTA LA TABLA PARA EVITAR QUE EL CONTENIDO SE DUPLIQUE*/
 
-function pintartabla(){
+const pintartabla = () => {
 bodytable.innerHTML = "";
 for(let filaValue of todosLosVerbos){
     const newfila = document.createElement("tr");
@@ -61,40 +66,64 @@ for(let filaValue of todosLosVerbos){
    bodytable.appendChild(newfila);
 }}
 
+    /*FILTRA LA TABLA EN ORDEN DE LA A-Z*/
+
 const filterAZ = document.getElementById("filtrarAZ");
 
-filterAZ.addEventListener("click", filtrartablaAZ);
-
-function filtrartablaAZ(){
+const filtrartablaAZ = () => {
    todosLosVerbos.sort((a, b) => a[0].localeCompare(b[0]));
    pintartabla();
 }
+filterAZ.addEventListener("click", filtrartablaAZ);
 
+    /*FILTRA LA TABLA EN ORDEN DE LA Z-A*/
 
 const filterZA = document.getElementById("filtrarZA");
 
-filterZA.addEventListener("click", filtrartablaZA);
-
-function filtrartablaZA(){
+const filtrartablaZA = () => {
    todosLosVerbos.sort((a, b) => b[0].localeCompare(a[0]));
    pintartabla();
 }
-    /*BUSCADOR DE VERBOS ESPECIFICOS*/
+filterZA.addEventListener("click", filtrartablaZA);
 
-searcher.addEventListener("input", filtrartabla);
+    /*FILTRA LA TABLA Y MUESTRA SOLO VERBOS REGULARES*/
 
-function filtrartabla(){
-    const lupa = searcher.value.toLowerCase();
-    const filas = tablee.getElementsByTagName("tr");
+    const filterR = document.getElementById("filtrarRegulares");
+    const filtrarxR = () => {
     for(let fila of filas){
         const celds = fila.getElementsByTagName("td");
         if(celds.length > 0){
             const verbo = celds[0].innerText.toLowerCase();
-            if(verbo.includes(lupa)){
-                fila.style.display = "";
-            }else{
-                fila.style.display = "none";
-            }
+            const esregular = arrayVerbosRegular.some(regular => regular[0].toLowerCase() === verbo);
+            fila.style.display = esregular ? "" : "none";
+        }
+    }
+    };
+  filterR.addEventListener("click", filtrarxR);
+
+        /*FILTRA LA TABLA Y MUESTRA SOLO VERBOS REGULARES*/
+        
+const filtrarIrregularess = document.getElementById("filtrarIrregulares");
+const filtradorirregulares = () => {
+    for(let fila of filas){
+        const celds = fila.getElementsByTagName("td");
+        if(celds.length > 0){
+            const verbo = celds[0].innerText.toLowerCase();
+            const esIrregular = arrayVerbosIrregular.some(irregular => irregular[0].toLowerCase() === verbo);
+            fila.style.display = esIrregular ? "" : "none";
+        }}}
+    filtrarIrregularess.addEventListener("click", filtradorirregulares);
+   
+  /*BUSCADOR DE VERBOS ESPECIFICOS*/
+
+const filtrartabla = () => {
+    const lupa = searcher.value.toLowerCase();
+        for(let fila of filas){
+ const celds = fila.getElementsByTagName("td");
+        if(celds.length > 0){
+            const verbo = celds[0].innerText.toLowerCase();
+            fila.style.display = verbo.includes(lupa) ? "" : "none"
             }
         }
     }
+    searcher.addEventListener("input", filtrartabla);
